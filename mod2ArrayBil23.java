bc3e2a8e088689d1382f193f0f0d37ba66c7a5ec
fri17da <syafridaadeauliahakim@gmail.com>
2024-11-06 12:30:25 +0700

    commit

---
+++ mod2ArrayBil23.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class mod2ArrayBil23 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] bil = new int[4];
+        for (int i = 0; i < 4; i++) {
+            System.out.println(bil[i]);
+        }
+        System.out.println();
+    }
+}
